8f7ea9bd65b0f1f34c80cd94b2c27bcf0a32cfac
Gihyun-developSpace <glenkoh@naver.com>
2021-10-10 21:41:54 +0900

    개인프로젝트 초기설정

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<p>send</p>
+	<div class="btn btn-primary">Hi</div>
+	<i class="bi bi-arrow-left-square"></i>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+p {
+	color: $primary;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>hi</div>
+</template>
+
+<script>
+export default {
+	setup() {
+		return {}
+	}
+}
+</script>
+
+<style lang="scss" scoped></style>
